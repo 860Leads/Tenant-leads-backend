6c32696e52305cf0b669de74f98570b539e8b853
860Leads <maria@860leads.com>
2026-07-21 22:11:51 -0400

    Create tenants.config.js

---
+++ config/tenants.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  TENANTS_DEFAULT_RENT: 1200,
+  TENANTS_INCOME_MULTIPLIER: 3,
+  TENANTS_MIN_TENURE_YEARS: 1,
+  TENANTS_IDEAL_MOVEOUT_DAYS: 30,
+  SCORING_WEIGHTS: {
+    income: 2,
+    tenure: 2,
+    timeline: 2
+  }
+};
